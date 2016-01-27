5d0177b8ab291f07f839e56f8f430dd3eb3804bc
Joe-noh <honzawa.j@pepabo.com>
2016-01-27 10:01:01 +0900

    web ui can be seen on localhost:4000/exq

--- web/router.ex
+++ web/router.ex
@@ -1,8 +1,22 @@
 defmodule Meli.Router do
   use Meli.Web, :router
 
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :put_secure_browser_headers
+    plug ExqUi.RouterPlug, namespace: "exq"
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  scope "/", Meli do
+    pipe_through :browser
+
+    forward "/exq", ExqUi.RouterPlug.Router, :index
   end
 
   scope "/api", Meli do
